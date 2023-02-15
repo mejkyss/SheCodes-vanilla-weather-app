b11bc2a4a26bbdd4ce6ec4de282fa39b63f81994
mejkyss <124203408+mejkyss@users.noreply.github.com>
2023-02-15 16:49:59 +0100

    Add weather icons & change them based on the weather

--- src/app.js
+++ src/app.js
@@ -1,26 +1,6 @@
 let apiKey = "f3b08f2120e855cotb88e29725334a5b";
 
-function searchForCity(event) {
-  event.preventDefault();
-  let searchField = document.querySelector("#search-field");
-  let newCityName = searchField.value;
-  let url = `https://api.shecodes.io/weather/v1/current?units=metric&query=${newCityName}&key=${apiKey}`;
-  axios.get(url).then(logCityValues).catch(catchError);
-}
-
-function logCityValues(response) {
-  let newTemperature = response.data.temperature.current;
-  let newCityName = response.data.city;
-  let oldCityName = document.querySelector("#city-name");
-  let oldTemperature = document.querySelector("#current-degrees");
-
-  oldCityName.innerHTML = newCityName;
-  oldTemperature.innerHTML = `${Math.round(newTemperature)}°C`;
-}
-function catchError(error) {
-  console.error(error);
-  alert("Sorry, but you need to spell the city correctly.");
-}
+//Main functions
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchForCity);
@@ -28,6 +8,30 @@ searchForm.addEventListener("submit", searchForCity);
 let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", searchForCity);
 
+function searchForCity(event) {
+  event.preventDefault();
+  let searchField = document.querySelector("#search-field");
+  let newCityName = searchField.value;
+  let url = `https://api.shecodes.io/weather/v1/current?units=metric&query=${newCityName}&key=${apiKey}`;
+  axios.get(url).then(replaceAppValues).catch(catchError);
+}
+function catchError(error) {
+  console.error(error);
+  alert("Sorry, but you need to spell the city correctly.");
+}
+
+function replaceAppValues(response) {
+  let newTemperature = response.data.temperature.current;
+  let newCityName = response.data.city;
+  let oldCityName = document.querySelector("#city-name");
+  let oldTemperature = document.querySelector("#current-degrees");
+  oldCityName.innerHTML = newCityName;
+  oldTemperature.innerHTML = `${Math.round(newTemperature)}°C`;
+  let weatherIcon = document.querySelector("#weather-icon");
+  weatherIcon.setAttribute("src", response.data.condition.icon_url);
+}
+
+// ----> Current location
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", buttonCurrentLocation);
 
@@ -40,5 +44,17 @@ function getLocation(position) {
   let lon = position.coords.longitude;
   let apiKey = "f3b08f2120e855cotb88e29725334a5b";
   let url = `https://api.shecodes.io/weather/v1/current?units=metric&key=${apiKey}&lon=${lon}&lat=${lat}`;
-  axios.get(url).then(logCityValues);
+  axios.get(url).then(replaceAppValues);
 }
+
+// Converting between Farenheit & Celsius
+
+// let celsiusButton = document.querySelector("#celsius-button");
+// celsiustButton.addEventListener("click", displayCelsius);
+
+// function displayCelsius() {}
+
+// let farenheitButton = document.querySelector("#farenheit-button");
+// farenheitButton.addEventListener("click", displayFarenheit);
+
+// function displayFarenheit() {}
